7b780637ec0f80b362057084f7b848a45cd7248d
khaledalawagy <148382056+khaledalawagy@users.noreply.github.com>
2024-05-09 16:06:33 +0300

    php
    php

---
+++ createAcc.php
@@ -0,0 +1,277 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $mobile = $_POST["mobile"];
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+    $repass = $_POST["password_confirmation"];
+    $names = explode(" ", $name);
+    $first_name = $names[0];
+    $last_name = end($names);
+
+    if ($pass != $repass) {
+        echo '<script> alert("Password doesn\'t match")</script>';
+    } else {
+        $sql = "SELECT * FROM customer WHERE Email = '$email' ";
+        $res = mysqli_query($db, $sql) or die(mysqli_error($db));
+        if (mysqli_num_rows($res) >= 1) {
+            echo '<script>alert("Email already taken")</script>';
+        } else {
+            $sql_cs = "INSERT INTO customer (First_Name, Last_Name, Email, Password) VALUES ('$first_name', '$last_name', '$email', '$pass')";
+            if (mysqli_query($db, $sql_cs)) {          
+                $sql_customer_id = "SELECT Customer_ID FROM customer WHERE Email = '$email'";
+                $result_customer_id = mysqli_query($db, $sql_customer_id);
+                $row_customer_id = mysqli_fetch_assoc($result_customer_id);
+                $customerID = $row_customer_id['Customer_ID'];
+                $sql_insert_phone = "INSERT INTO phone_number (Customer,Phone_No) VALUES ('$customerID','$mobile')";
+                if (!mysqli_query($db, $sql_insert_phone)) {
+                    echo "Error: " . $sql_insert_phone . "<br>" . mysqli_error($db);
+                }
+            } else {
+                echo "Error: " . $sql_cs. "<br>" . mysqli_error($db);
+            }
+        }
+    }
+}
+header("location:loginPage.php");
+exit();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+    <link rel="stylesheet" href="createAcc.css" />
+    <link rel="stylesheet" href="hhhhhhhh.css" />
+    <link rel="stylesheet" href="footer.css" />
+    <title>Sign Up</title>
+  </head>
+  <body>
+    <header style="background-color: transparent">
+      <div class="logo">
+        <h2 id="CinemaName" style="cursor: pointer">OCinema</h2>
+      </div>
+      <nav class="navigation">
+        <a href="home.html">Now Showing</a>
+        <a href="comingsoon.html">Coming Soon</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+      <div class="login">
+        <button id="myaccButton">MY ACCOUNT</button>
+        <button id="loginnButton"><i class="fas fa-user"></i> Login</button>
+      </div>
+    </header>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("loginnButton")
+          .addEventListener("click", function () {
+            window.location.href = "loginPage.html";
+          });
+      });
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("CinemaName")
+          .addEventListener("click", function () {
+            window.location.href = "home.html";
+          });
+      });
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("myaccButton")
+          .addEventListener("click", function () {
+            window.location.href = "myAcc.html";
+          });
+      });
+
+      //       document.getElementById("login-form").addEventListener("submit", function(event) {
+      //     event.preventDefault();
+
+      //     // Get user input
+      //     var email = document.getElementById("email").value;
+      //     var password = document.getElementById("password").value;
+
+      //     // Store user data securely in localStorage
+      //     localStorage.setItem("userEmail", email);
+      //     localStorage.setItem("userPassword", password);
+
+      //     // Redirect to login page
+      //     window.location.href = "loginPage.html";
+      // });
+
+      document
+        .getElementById("login-form")
+        .addEventListener("submit", function (event) {
+          event.preventDefault();
+          var name = document.getElementById("name").value;
+          localStorage.setItem("name", name);
+          window.location.href = "loginPage.html";
+        });
+    </script>
+    <div class="">
+      <div class="container">
+        <div class="left-section">
+          <div>
+            <p style="margin-left: 250px; font-weight: 600">
+              Join us today and experience the magic of cinema like never
+              before!
+            </p>
+            <div class="maintagcontact" style="width: 100%; text-align: center">
+              <img src="chair.jpg" width="265;" />
+            </div>
+          </div>
+        </div>
+        <div class="right-section">
+          <div class="signup-container">
+            <div class="maintag mb-3">
+              <h3 style="padding-bottom: 15px">Create An Account</h3>
+            </div>
+            <form
+                method="post"
+              action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+              class="register-form"
+              id="login-form" 
+            >
+              <div class="row">
+                <div class="your-name">
+                  <label for="name"></label>
+                  <input
+                    type="text"
+                    name="name"
+                    class="rns-form-element"
+                    id="name"
+                    value=""
+                    placeholder="Your Name"
+                    required
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <input
+                    type="text"
+                    name="mobile"
+                    class="rns-form-element"
+                    id="mobile"
+                    value=""
+                    placeholder="Your Mobile Number"
+                    required
+                  />
+                </div>
+                <div class="form-group col-md-12">
+                  <label for="email"></label>
+                  <input
+                    type="email"
+                    name="email"
+                    class="rns-form-element"
+                    id="email"
+                    value=""
+                    placeholder="Your Email"
+                    required
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="pass"></label>
+                  <input
+                    type="password"
+                    name="password"
+                    class="rns-form-element"
+                    id="password"
+                    value=""
+                    placeholder="Your Password"
+                    required
+                  />
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="re-pass"></label>
+                  <input
+                    type="password"
+                    class="rns-form-element"
+                    name="password_confirmation"
+                    id="password_confirmation"
+                    placeholder="Repeat your password"
+                    required
+                  />
+                </div>
+              </div>
+              <div
+                class="form-group form-button"
+                style="margin-bottom: 15px; margin-top: 10px"
+              >
+                <input
+                  type="submit"
+                  name="register"
+                  id="register"
+                  class="btn_primary"
+                  value="Register"
+                />
+              </div>
+            </form>
+            <div class="row">
+              <div
+                class="col-md-12 col-sm-12 text-center mb-2 sub_notes"
+                style="margin-left: 100px; margin-bottom: 20px"
+              >
+                Do you have an account?
+                <a href="loginPage.html" class="purple-link">Login Now</a>
+              </div>
+              <div class="col-md-12 col-sm-12 text-center sub_notes mt-2">
+                <div class="social-login">
+                  <ul class="social_login">
+                    <li class="pt-1 orc">
+                      <b><span style="color: #a02d8a">OR CONNECT WITH</span></b>
+                    </li>
+                    <li>
+                      <a href="https://google.com">
+                        <img
+                          src="https://rnscinemas.net/public/ui/images/checkout/google.png"
+                          class="social-icon"
+                        />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://twitter.com">
+                        <img
+                          src="https://rnscinemas.net/public/ui/images/checkout/twitter.png"
+                          class="social-icon"
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <hr style="opacity: 0.5" />
+      <div class="footer-container">
+        <div class="footer-icons">
+          <a href=""><i class="fab fa-facebook-f"></i></a>
+          <a href=""><i class="fab fa-instagram"></i></a>
+          <a href=""><i class="fab fa-tiktok"></i></a>
+        </div>
+        <div class="footer-nav">
+          <ul>
+            <ul>
+              <li><a href="aboutus.html">About us</a></li>
+              <li><a href="privacy.html">Privacy Policy</a></li>
+              <li><a href="refunds.html">Refunds & Exchanges</a></li>
+              <li><a href="termsofservice.html">Terms of Service</a></li>
+            </ul>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p style="color: #a02d8a">
+          &copy; 2024 All right reserved by OCinema
+        </p>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ loginPage.php
@@ -0,0 +1,237 @@
+<?php 
+session_start();
+include("dbcon.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST['email']) && isset($_POST['password'])){
+        function validate($data){
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+        
+       $email = validate($_POST['email']);
+        $pass = validate($_POST['password']);
+        $query = "SELECT * FROM customer WHERE Email='{$_SESSION['email']}' AND Password='$pass'";
+        $result = mysqli_query($db, $query);
+        
+        if(mysqli_num_rows($result) === 1){
+            $row = mysqli_fetch_assoc($result);
+            if($row['Email'] === $email && $row['Password'] === $pass){
+                header("Location:home.html");
+                exit(); 
+            }
+        } else {
+            echo "<script>alert('Invalid Email/Password');</script>";
+            header("Location:loginPage.php"); 
+            exit(); 
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+    <link rel="stylesheet" href="loginPage.css" />
+    <link rel="stylesheet" href="hhhhhhhh.css" />
+    <title>Sign In</title>
+  </head>
+  <body>
+    <header style="background-color: transparent">
+      <div class="logo">
+        <h2 id="CinemaName" style="cursor: pointer">OCinema</h2>
+      </div>
+
+      <nav class="navigation">
+        <a href="home.html">Now Showing</a>
+        <a href="comingsoon.html">Coming Soon</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+      <div class="login">
+        <button id="myaccButton">MY ACCOUNT</button>
+        <button id="loginnButton"><i class="fas fa-user"></i> Login</button>
+      </div>
+    </header>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("loginnButton")
+          .addEventListener("click", function () {
+            window.location.href = "loginPage.html";
+          });
+      });
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("CinemaName")
+          .addEventListener("click", function () {
+            window.location.href = "home.html";
+          });
+      });
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("myaccButton")
+          .addEventListener("click", function () {
+            window.location.href = "myAcc.html";
+          });
+      });
+    </script>
+    <div class="">
+      <div class="container">
+        <div class="left-section">
+          <div class="inner-content">
+            <p style="text-align: center; font-weight: 600; margin-left: 270px">
+              Welcome to Renaissance Cinemas, where you can enjoy the best
+              movies
+            </p>
+            <p style="text-align: center; font-weight: 600; margin-left: 270px">
+              from around the world in a comfortable and elegant setting.
+            </p>
+            <div class="maintagcontact" style="width: 100%; text-align: center">
+              <img src="chair.jpg" width="265" />
+            </div>
+          </div>
+        </div>
+        <div class="right-section">
+          <div class="signin-form">
+            <h2
+              style="
+                margin-bottom: 30px;
+                font-family: Verdana, Geneva, Tahoma, sans-serif;
+              "
+            >
+              Sign in
+            </h2>
+            <form method="post" id="login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+              <input
+                type="hidden"
+                name="_token"
+                value="pWq4HX8tMeM8xB9QO4YlZLjRhZ6wedVNy0jEgqsq"
+              />
+              <div class="form-group">
+                <label for="email"><i class=""></i></label>
+                <input
+                  id="email"
+                  name="email"
+                  type="text"
+                  class="textelement"
+                  value=""
+                  required
+                  placeholder="Email Or Phone Number"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  class="textelement"
+                  placeholder="Your Password"
+                  required
+                />
+              </div>
+              <div
+                class="form-group text-right"
+                style="
+                  text-align: right;
+                  font-size: 14px;
+                  margin-top: 5px;
+                  margin-bottom: 25px;
+                "
+              >
+                <input type="checkbox" name="remember" id="remember" class="" />
+                <label for="remember-me" class=""
+                  ><span><span></span></span>Remember me</label
+                >
+              </div>
+              <div class="" style="margin-bottom: 20px">
+                <input
+                  type="submit"
+                  name="signin"
+                  id="signin"
+                  class="btn_primary"
+                  value="Sign in"
+                />
+              </div>
+            </form>
+            <div class="row" id="roww">
+              <div class="">
+                <span class="dont-have-account"
+                  >Don't have an account?
+                  <a
+                    href="createAcc.html"
+                    class="create-account"
+                    style="text-decoration: none"
+                    >Create An Account</a
+                  >
+                  -
+                  <a
+                    href="passReset.html"
+                    class="forgot-password"
+                    style="text-decoration: none"
+                    >Forgot Password?</a
+                  ></span
+                >
+              </div>
+              <div class="">
+                <div class="social-login">
+                  <ul class="social_login no-bullet">
+                    <li class="pt-1 orc">
+                      <b
+                        ><span style="color: #a02d8a; padding-left: 25px"
+                          >OR CONNECT WITH</span
+                        ></b
+                      >
+                    </li>
+                    <li>
+                      <a href="https://google.com/"
+                        ><img src="google.png" class="social-icon"
+                      /></a>
+                    </li>
+                    <li>
+                      <a href="https://twitter.com"
+                        ><img src="twitter.png" class="social-icon"
+                      /></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <hr style="opacity: 0.5" />
+      <div class="footer-container">
+        <div class="footer-icons">
+          <a href=""><i class="fab fa-facebook-f"></i></a>
+          <a href=""><i class="fab fa-instagram"></i></a>
+          <a href=""><i class="fab fa-tiktok"></i></a>
+        </div>
+        <div class="footer-nav">
+          <ul>
+            <li><a href="aboutus.html">About us</a></li>
+            <li><a href="privacy.html">Privacy Policy</a></li>
+            <li><a href="refunds.html">Refunds & Exchanges</a></li>
+            <li><a href="termsofservice.html">Terms of Service</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p style="color: #a02d8a">
+          &copy; 2024 All rights reserved by OCinema
+        </p>
+      </div>
+    </footer>
+  </body>
+</html>
