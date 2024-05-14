1fc2e522fa77134815ab79803946a0a3f1e0b06b
khaledalawagy <148382056+khaledalawagy@users.noreply.github.com>
2024-05-14 17:42:06 +0300

    a
    a

--- createAcc.php
+++ createAcc.php
@@ -1,5 +1,6 @@
 <?php
 require "dbcon.php";
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = $_POST["name"];
     $mobile = $_POST["mobile"];
@@ -20,18 +21,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             echo '<script>alert("Email already taken")</script>';
         } else { 
           mysqli_query($db,"insert into customer (First_Name, Last_Name, Email, Password) values ('$first_name', '$last_name', '$email', '$pass')");
-          require "loginPage.php";
-            
+          header( "location:loginPage.php");
+            exit;
             } 
 }
-}?>
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8"/>
+    <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
     <link rel="stylesheet" href="createAcc.css" />
     <link rel="stylesheet" href="hhhhhhhh.css" />
     <link rel="stylesheet" href="footer.css" />
@@ -52,12 +58,60 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <button id="loginnButton"><i class="fas fa-user"></i> Login</button>
       </div>
     </header>
-    <div class="">
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("loginnButton")
+          .addEventListener("click", function () {
+            window.location.href = "loginPage.php";
+          });
+      });
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("CinemaName")
+          .addEventListener("click", function () {
+            window.location.href = "home.html";
+          });
+      });
+      document.addEventListener("DOMContentLoaded", function () {
+        document
+          .getElementById("myaccButton")
+          .addEventListener("click", function () {
+            window.location.href = "myAcc.html";
+          });
+      });
+
+      //       document.getElementById("login-form").addEventListener("submit", function(event) {
+      //     event.preventDefault();
+
+      //     // Get user input
+      //     var email = document.getElementById("email").value;
+      //     var password = document.getElementById("password").value;
+
+      //     // Store user data securely in localStorage
+      //     localStorage.setItem("userEmail", email);
+      //     localStorage.setItem("userPassword", password);
+
+      //     // Redirect to login page
+      //     window.location.href = "loginPage.html";
+      // });
+
+      document
+        .getElementById("login-form")
+        .addEventListener("submit", function (event) {
+          event.preventDefault();
+          var name = document.getElementById("name").value;
+          localStorage.setItem("name", name);
+          window.location.href = "loginPage.html";
+        });
+    </script>
+    <div class="the_body m-0 p-0">
       <div class="container">
         <div class="left-section">
           <div>
             <p style="margin-left: 250px; font-weight: 600">
-              Join us today and experience the magic of cinema like neverbefore!
+              Join us today and experience the magic of cinema like never
+              before!
             </p>
             <div class="maintagcontact" style="width: 100%; text-align: center">
               <img src="chair.jpg" width="265;" />
@@ -70,35 +124,91 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
               <h3 style="padding-bottom: 15px">Create An Account</h3>
             </div>
             <form
-              method="POST"action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"class="register-form"id="login-form">
+              method="POST"
+              action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+              class="register-form"
+              id="login-form"
+            >
               <div class="row">
                 <div class="your-name">
                   <label for="name"></label>
-                  <input type="text"name="name"class="rns-form-element"id="name"value=""placeholder="Your Name"required/>
+                  <input
+                    type="text"
+                    name="name"
+                    class="rns-form-element"
+                    id="name"
+                    value=""
+                    placeholder="Your Name"
+                    required
+                  />
                 </div>
                 <div class="form-group col-md-6">
-                  <input type="text"name="mobile"class="rns-form-element"id="mobile"value=""placeholder="Your Mobile Number"required maxlength="11"/>
+                  <input
+                    type="text"
+                    name="mobile"
+                    class="rns-form-element"
+                    id="mobile"
+                    value=""
+                    placeholder="Your Mobile Number"
+                    required
+                  />
                 </div>
                 <div class="form-group col-md-12">
                   <label for="email"></label>
-                  <input type="email"name="email"class="rns-form-element"id="email"value=""placeholder="Your Email"required/>
+                  <input
+                    type="email"
+                    name="email"
+                    class="rns-form-element"
+                    id="email"
+                    value=""
+                    placeholder="Your Email"
+                    required
+                  />
                 </div>
                 <div class="form-group col-md-6">
                   <label for="pass"></label>
-                  <input type="password"name="password"class="rns-form-element"id="password"value=""placeholder="Your Password"required/>
+                  <input
+                    type="password"
+                    name="password"
+                    class="rns-form-element"
+                    id="password"
+                    value=""
+                    placeholder="Your Password"
+                    required
+                  />
                 </div>
                 <div class="form-group col-md-6">
                   <label for="re-pass"></label>
-                  <input type="password"class="rns-form-element"name="password_confirmation"id="password_confirmation"placeholder="Repeat your password"required/>
+                  <input
+                    type="password"
+                    class="rns-form-element"
+                    name="password_confirmation"
+                    id="password_confirmation"
+                    placeholder="Repeat your password"
+                    required
+                  />
                 </div>
               </div>
-              <div class="form-group form-button"style="margin-bottom: 15px; margin-top: 10px">
-                <input type="submit"name="register"id="register"class="btn_primary"value="Register"/>
+              <div
+                class="form-group form-button"
+                style="margin-bottom: 15px; margin-top: 10px"
+              >
+                <input
+                  type="submit"
+                  name="register"
+                  id="register"
+                  class="btn_primary"
+                  value="Register"
+                />
               </div>
             </form>
             <div class="row">
-              <div class="col-md-12 col-sm-12 text-center mb-2 sub_notes"style="margin-left: 100px; margin-bottom: 20px">Do you have an account?
-              <a href="loginPage.html" class="purple-link">Login Now</a>
+              <div
+                class="col-md-12 col-sm-12 text-center mb-2 sub_notes"
+                style="margin-left: 100px; margin-bottom: 20px"
+              >
+                Do you have an account?
+                <a href="loginPage.html" class="purple-link">Login Now</a>
               </div>
               <div class="col-md-12 col-sm-12 text-center sub_notes mt-2">
                 <div class="social-login">
@@ -108,12 +218,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </li>
                     <li>
                       <a href="https://google.com">
-                        <img src="https://rnscinemas.net/public/ui/images/checkout/google.png"class="social-icon"/>
+                        <img
+                          src="https://rnscinemas.net/public/ui/images/checkout/google.png"
+                          class="social-icon"
+                        />
                       </a>
                     </li>
                     <li>
                       <a href="https://twitter.com">
-                        <img src="https://rnscinemas.net/public/ui/images/checkout/twitter.png"class="social-icon"/>
+                        <img
+                          src="https://rnscinemas.net/public/ui/images/checkout/twitter.png"
+                          class="social-icon"
+                        />
                       </a>
                     </li>
                   </ul>
@@ -124,42 +240,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </div>
       </div>
     </div>
-    <script>
-      document.addEventListener("DOMContentLoaded", function () {
-        document.getElementById("loginnButton").addEventListener("click", function () {
-            window.location.href = "loginPage.php";
-          });
-      });
-      document.addEventListener("DOMContentLoaded", function () {
-        document.getElementById("CinemaName").addEventListener("click", function () {
-            window.location.href = "home.html";
-          });
-      });
-      document.addEventListener("DOMContentLoaded", function () {
-        document.getElementById("myaccButton").addEventListener("click", function () {
-            window.location.href = "myAcc.html";
-          });
-      });
-
-      //       document.getElementById("login-form").addEventListener("submit", function(event) {
-      //     event.preventDefault();
-      //     // Get user input
-      //     var email = document.getElementById("email").value;
-      //     var password = document.getElementById("password").value;
-      //     // Store user data securely in localStorage
-      //     localStorage.setItem("userEmail", email);
-      //     localStorage.setItem("userPassword", password);
-      //     // Redirect to login page
-      //     window.location.href = "loginPage.html";
-      // });
-
-        document.getElementById("login-form").addEventListener("submit", function (event) {
-          event.preventDefault();
-          var name = document.getElementById("name").value;
-          localStorage.setItem("name", name);
-          window.location.href = "loginPage.php";
-        });
-    </script>
     <footer>
       <hr style="opacity: 0.5" />
       <div class="footer-container">
@@ -185,5 +265,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         </p>
       </div>
     </footer>
-  </body>
+  </body>
 </html>
